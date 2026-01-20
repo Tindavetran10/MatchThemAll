dc0a867857ab79c8061f3604a3e445ee8e0bab39
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2026-01-20 23:17:34 +0700

    Goal Cards

---
+++ Assets/Match Them All/Scripts/Managers/GoalManager.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using MatchThemAll.Scripts;
+using MatchThemAll.Scripts.UI;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+    [Header(" References ")]
+    [SerializeField] private Transform goalCardParent;
+    [SerializeField] private GoalCard goalCardPrefab;
+    
+    [Header(" Data ")]
+    private ItemLevelData[] _goals;
+    private List<GoalCard> goalCards = new List<GoalCard>();
+    
+    private void Awake()
+    {
+        LevelManager.levelSpawned += OnLevelSpawned;
+        ItemSpotManager.ItemPickedUp += OnItemPickedUp;
+    }
+
+    private void OnDestroy()
+    {
+        LevelManager.levelSpawned -= OnLevelSpawned;
+        ItemSpotManager.ItemPickedUp -= OnItemPickedUp;
+    }
+    
+    private void OnLevelSpawned(Level level)
+    {
+        _goals = level.GetGoals();
+
+        GenerateGoalCards();
+    }
+
+    private void GenerateGoalCards()
+    {
+        foreach (var goal in _goals) 
+            GenerateGoalCard(goal);
+    }
+
+    private void GenerateGoalCard(ItemLevelData goal)
+    {
+        GoalCard cardInstance = Instantiate(goalCardPrefab, goalCardParent);
+        
+        cardInstance.Configure(goal.amount);
+        goalCards.Add(cardInstance);
+    }
+
+    private void OnItemPickedUp(Item item)
+    {
+        for (var i = 0; i < _goals.Length; i++)
+        {
+            if(!_goals[i].itemPrefab.ItemNameKey.Equals(item.ItemNameKey))
+                continue;
+            
+            _goals[i].amount--;
+
+            if (_goals[i].amount <= 0)
+                CompleteGoals(i);
+            else
+                goalCards[i].UpdateAmount(_goals[i].amount);
+            
+            break;
+        }
+    }
+    
+    private void CompleteGoals(int goalIndex)
+    {
+        Debug.Log($"Goal {goalIndex} completed!");
+        goalCards[goalIndex].Complete();
+        CheckForLevelComplete();
+    }
+
+    private void CheckForLevelComplete()
+    {
+        for (var i = 0; i < _goals.Length; i++)
+        {
+            if(_goals[i].amount > 0)
+                return;
+        }
+        
+        Debug.Log("Level Complete!");
+    }
+}
---
+++ Assets/Match Them All/Scripts/UI/GoalCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace MatchThemAll.Scripts.UI
+{
+    public class GoalCard : MonoBehaviour
+    {
+       [Header(" Elements ")]
+       [SerializeField] private TextMeshProUGUI amountText;
+
+       private void Start()
+       {
+           
+       }
+
+       private void Update()
+       {
+           
+       }
+
+       public void Configure(int initialAmount)
+       {
+           amountText.text = initialAmount.ToString();
+       }
+
+       public void UpdateAmount(int amount)
+       {
+           amountText.text = amount.ToString();
+       }
+
+       public void Complete()
+       {
+           gameObject.SetActive(false);
+       }
+    }
+}
