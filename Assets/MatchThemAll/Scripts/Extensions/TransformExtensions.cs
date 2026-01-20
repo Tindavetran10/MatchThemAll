c6acd9e1c53f1e69b77d8a5d9e60e269c7a6650a
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2026-01-20 21:52:41 +0700

    Level Manager

---
+++ Assets/MatchThemAll/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MatchThemAll.Scripts.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void Clear(this Transform transform)
+        {
+            while (transform.childCount > 0)
+            {
+                Transform child = transform.GetChild(0);
+                child.parent = null;
+                Object.Destroy(child.gameObject);
+            }
+        }
+        
+        public static Transform GetLast(this Transform transform)
+        {
+            if (transform.childCount <= 0) return null;
+            
+            return transform.GetChild(transform.childCount - 1);
+        }
+    }
+}
