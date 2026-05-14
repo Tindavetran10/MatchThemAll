b9b3327ffc68d27e43b8333e0648bbe9eaa058f0
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2026-05-14 23:52:02 +0700

    Add goal and goal cards animation

--- Assets/Match Them All/Scripts/UI/GoalCard.cs
+++ Assets/Match Them All/Scripts/UI/GoalCard.cs
@@ -9,6 +9,19 @@ namespace MatchThemAll.Scripts.UI
        [Header(" Elements ")]
        [SerializeField] private Image iconImage;
        [SerializeField] private TextMeshProUGUI amountText;
+       [SerializeField] private GameObject checkMark;
+       [SerializeField] private GameObject backFace;
+       [SerializeField] private Animator animator;
+
+       private void Start()
+       {
+           animator.enabled = false;
+       }
+
+       private void Update()
+       {
+           backFace.SetActive(Vector3.Dot(Vector3.forward, transform.forward) < 0);
+       }
        
        public void Configure(int initialAmount, Sprite icon)
        {
@@ -19,11 +32,24 @@ namespace MatchThemAll.Scripts.UI
        public void UpdateAmount(int amount)
        {
            amountText.text = amount.ToString();
+           Bump();
+       }
+
+       private void Bump()
+       {
+           LeanTween.cancel(gameObject);
+           transform.localScale = Vector3.one;
+           LeanTween.scale(gameObject, Vector3.one * 1.1f, 0.25f).setLoopPingPong(1);
        }
 
        public void Complete()
        {
-           gameObject.SetActive(false);
+           //gameObject.SetActive(false);
+           animator.enabled = true;
+           checkMark.SetActive(true);
+           amountText.text = "";
+           
+           animator.Play("Complete");
        }
     }
 }
